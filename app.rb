8dcb84574e31050878abbaaefb1d1ea5053c0d73
Mark Viti <+>
2022-06-21 00:08:36 -0400

    commit tree e7f3f90fe00e6d03ca4bcd22d64110f624fd0b95

--- app.rb
+++ app.rb
@@ -1,6 +1,49 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  get "/reversename/:name" do
+    @name = params[:name]
+    @name.reverse
+  end 
+
+  get "/square/:number" do 
+    @number = params[:number].to_f
+    "#{@number**2}"
+  end 
+
+  get '/say/:number/:phrase' do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+
+    @phrase* @number
+  end 
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+  
+    @word1+ " " +@word2+ " " +@word3+ " " +@word4+ " " +@word5+"."
+  end
+  
+  get '/:operation/:number1/:number2' do 
+    if params[:operation] == "add" 
+        @operation = "+"
+    elsif params[:operation] == "subtract" 
+      @operation = "-"
+    elsif params[:operation] == "divide"
+      @operation = "/"
+    elsif params[:operation] == "multiply"
+      @operation = "*"
+    end
+
+    @number1 = params[:number1].to_f
+    @number2 = params[:number2].to_f
+
+    @number1.send(@operation, @number2).to_s
+
+  end
 end
